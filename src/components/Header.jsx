442724c65d7c5349c16188dc8c044f8a42de5943
Kostiantyn Popravka <popravka1209@gmail.com>
2023-02-26 11:36:57 +0100

    adding existing repository

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className='header'>
+            <div>WelcUm</div>
+            {/*<img src = 'https://i.pinimg.com/originals/55/d2/5d/55d25d804be9260fd4212943b4b9c7ae.gif'></img>*/}
+        </header>
+    );
+}
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,8 @@
+const Profile = () => {
+    return(<div className='content'>
+        <div>
+            {/*<img src='https://64.media.tumblr.com/b7ada99b857f43494c72a159429e65fe/11629f9d223b201a-97/s1280x1920/ecc562fe403df410e8481a7379ccf0e5d6f262ec.gif'></img>*/}
+        </div>
+    </div>);
+}
+export default Profile;
