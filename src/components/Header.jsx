6998b8af6f89cb93ed0e0c7c06e7bca3997de5c7
Kostiantyn Popravka <popravka1209@gmail.com>
2023-02-26 20:04:41 +0100

    divided css to different modules
    exampe: Navbar.module.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,8 @@
+import classes from './Header.module.css'
 const Header = () => {
     return (
-        <header className='header'>
-            <div>WelcUm</div>
+        <header className={classes.header}>
+            <div className={classes.item }>WelcUm</div>
             {/*<img src = 'https://i.pinimg.com/originals/55/d2/5d/55d25d804be9260fd4212943b4b9c7ae.gif'></img>*/}
         </header>
     );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,6 @@
+import classes from './Profile.module.css'
 const Profile = () => {
-    return(<div className='content'>
+    return(<div className={classes.content}>
         <div>
             {/*<img src='https://64.media.tumblr.com/b7ada99b857f43494c72a159429e65fe/11629f9d223b201a-97/s1280x1920/ecc562fe403df410e8481a7379ccf0e5d6f262ec.gif'></img>*/}
         </div>
