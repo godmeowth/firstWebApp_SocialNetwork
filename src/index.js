296742ef8e274f1c45ee85d617e76db98ad418aa
Kostiantyn Popravka <popravka1209@gmail.com>
2023-03-15 20:16:06 +0100

    refactored state.js to OOP structure

--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import ReactDOM from "react-dom/client";
 import React from "react";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import state, {addPost, subscribe, updateNewPostText} from "./redux/state";
+import store from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 let rerenderEntireTree = () => {
     root.render(
         <React.StrictMode>
-            <App state = {state} addPost = {addPost} updateNewPostText = {updateNewPostText}/>
+            <App state = {store.getState()}
+                 addPost = {store.addPost.bind(store)}
+                 updateNewPostText = {store.updateNewPostText.bind(store)}/>
         </React.StrictMode>
     );
     reportWebVitals();
 }
-rerenderEntireTree(state)
-subscribe(rerenderEntireTree)
+rerenderEntireTree(store.getState())
+store.subscribe(rerenderEntireTree)
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,44 +1,48 @@
-let rerenderEntireTree = () => {
-    console.log('tree rerendered')
-}
-let state = {
-    postsPage:{
-        posts: [
-            {id: 1,  message: 'Hi everyone, Who know good psychologist', likesCount: 12},
-            {id: 2, message: 'WASSAP, I`VE JUST ATE MY MOTHER', likesCount: 20}
-        ],
-        newPostText: ''
+let store = {
+    _state: {
+        postsPage: {
+            posts: [
+                {id: 1, message: 'Hi everyone, Who know good psychologist', likesCount: 12},
+                {id: 2, message: 'WASSAP, I`VE JUST ATE MY MOTHER', likesCount: 20}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            messages: [
+                {id: 1, message: 'Hello Boba'},
+                {id: 2, message: 'BOBUS'},
+            ],
+            dialogs: [
+                {id: 1, name: 'Tokhir'},
+                {id: 2, name: 'Limon'},
+                {id: 3, name: 'Sheva'},
+            ],
+        },
     },
-    dialogsPage:{
-        messages: [
-            {id: 1, message: 'Hello Boba'},
-            {id: 2, message: 'BOBUS' },
-        ],
-        dialogs: [
-            {id: 1, name: 'Tokhir'},
-            {id: 2, name: 'Limon'},
-            {id: 3, name: 'Sheva'},
-        ],
+    getState(){
+        return this._state
     },
+    _callSubscriber() {
+        console.log('tree rerendered')
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.postsPage.newPostText,
+            likesCount: 0,
+        };
+        this._state.postsPage.posts.push(newPost);
+        this._state.postsPage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.postsPage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+      subscribe(observer) {
+        this._callSubscriber= observer
+      },
 }
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.postsPage.newPostText,
-        likesCount: 0,
-    };
-    state.postsPage.posts.push(newPost);
-    state.postsPage.newPostText = ''
-    rerenderEntireTree(state)
-}
-
-export let updateNewPostText = (newText) => {
-    state.postsPage.newPostText = newText;
-    rerenderEntireTree(state)
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer
-}
-export default state;
+export default store
+window.store = store
