c33d9d233161a51b02a33fea162be2a4b18186c8
Kostiantyn Popravka <popravka1209@gmail.com>
2023-04-01 20:46:02 +0200

    added DidMount method

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,9 +4,7 @@ import axios from "axios";
 import UserPhoto from "../../assets/images/UsersICO.png"
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props);
+    componentDidMount() {
         axios
             .get("https://social-network.samuraijs.com/api/1.0/users")
             .then((response) => {
