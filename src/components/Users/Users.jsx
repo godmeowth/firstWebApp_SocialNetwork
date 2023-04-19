b08635eeeb2629bde5278d8614899beebb965a4b
Kostiantyn Popravka <popravka1209@gmail.com>
2023-04-19 18:35:53 +0200

    follow/unfollow now working with axios http post and delete requests

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./Users.module.css";
 import UserPhoto from "../../assets/images/UsersICO.png";
 import {NavLink} from "react-router-dom";
+import axios from "axios";
 
 let Users = (props) => {
     let pageCount = Math.ceil(props.totalUserCount / props.pageSize)
@@ -28,9 +29,27 @@ let Users = (props) => {
                         </div>
                         <div>
                           {u.followed ? (
-                              <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
+                              <button onClick={() => {
+                                  axios
+                                      .delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {withCredentials: true})
+                                      .then((response) => {
+                                          if(response.data.resultCode == 0){
+                                              props.unfollow(u.id)
+                                          }
+                                      });
+
+
+                              }}>Unfollow</button>
                           ) : (
-                              <button onClick={() => props.follow(u.id)}>Follow</button>
+                              <button onClick={() =>{
+                                  axios
+                                      .post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {withCredentials: true})
+                                      .then((response) => {
+                                        if(response.data.resultCode == 0){
+                                            props.follow(u.id)
+                                        }
+                                      });
+                              }}>Follow</button>
                           )}
                         </div>
                       </span>
