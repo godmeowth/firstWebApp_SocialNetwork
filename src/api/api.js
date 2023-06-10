fb3e12a044b3f33216e13f5eca1773ae7a1ce172
Kostiantyn Popravka <popravka1209@gmail.com>
2023-06-10 18:58:01 +0200

    solved bug with login

--- src/api/api.js
+++ src/api/api.js
@@ -30,7 +30,7 @@ export const authAPI = {
         return instance.get(`auth/me`)
     },
     login(email, password, rememberMe = false) {
-        return instance.put(`auth/login`, {email, password, rememberMe})
+        return instance.post(`auth/login`, {email, password, rememberMe})
     },
     logout(){
         return instance.delete(`auth/login`)
