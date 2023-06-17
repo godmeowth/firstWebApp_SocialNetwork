c696b1ccd4081c1ebfca22194bf1b8eefb27104b
Kostiantyn Popravka <popravka1209@gmail.com>
2023-06-17 21:27:16 +0200

    started to add hooks (useState in profleStatus)

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import classes from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from "./ProfileStatus";
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
     if(!props.profile){
@@ -9,7 +10,7 @@ const ProfileInfo = (props) => {
     return (
         <div className={classes.profile}>
             <img src={props.profile.photos.large} ></img>
-            <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     );
 }
